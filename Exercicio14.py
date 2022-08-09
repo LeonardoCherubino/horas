7c955da38b286158815d741fdb2f0855cf05e8ad
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-08-08 21:44:23 -0300

    Horas Trabalhadas

---
+++ Exercicio14.py
@@ -0,0 +1,5 @@
+# 15. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+SalarioHora = float(input('Quanto você ganha por hora? '))
+HorasMes = float(input('Quantas horas você trabalha por mês? '))
+print(SalarioHora)
+print(HorasMes)
